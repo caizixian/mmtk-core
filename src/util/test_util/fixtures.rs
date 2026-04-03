08db866eadfa82f48ba2f83da0610917554a6e1b
Zixian Cai <zixianc@google.com>
2026-04-03 14:48:53 +1100

    Remove unsound blanket unsafe impl Sync for Fixture in src/util/test_util/fixtures.rs

--- src/util/test_util/fixtures.rs
+++ src/util/test_util/fixtures.rs
@@ -21,7 +21,6 @@ pub struct Fixture<T: FixtureContent> {
     once: Once,
 }
 
-unsafe impl<T: FixtureContent> Sync for Fixture<T> {}
 
 impl<T: FixtureContent> Fixture<T> {
     pub fn new() -> Self {
