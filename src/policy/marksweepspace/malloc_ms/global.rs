d5c64cc541e568599cf31cdbe7714178261afd73
Zixian Cai <zixianc@google.com>
2026-04-03 13:54:03 +1100

    Replace unsafe is_marked_unsafe with safe is_marked in MallocSpace sweep.

--- src/policy/marksweepspace/malloc_ms/global.rs
+++ src/policy/marksweepspace/malloc_ms/global.rs
@@ -615,8 +615,8 @@ impl<VM: VMBinding> MallocSpace<VM> {
     fn sweep_object(&self, object: ObjectReference, empty_page_start: &mut Address) -> bool {
         let (obj_start, offset_malloc, bytes) = Self::get_malloc_addr_size(object);
 
-        // We are the only thread that is dealing with the object. We can use non-atomic methods for the metadata.
-        if !unsafe { is_marked_unsafe::<VM>(object) } {
+        // We can use relaxed atomic loads to avoid unsafe blocks.
+        if !is_marked::<VM>(object, Ordering::Relaxed) {
             // Dead object
             trace!("Object {} has been allocated but not marked", object);
 
@@ -778,7 +778,7 @@ impl<VM: VMBinding> MallocSpace<VM> {
                     }
 
                     debug_assert!(
-                        unsafe { is_marked_unsafe::<VM>(object) },
+                        is_marked::<VM>(object, Ordering::Relaxed),
                         "Dead object = {} found after sweep",
                         object
                     );
